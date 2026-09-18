d651a3c8ddec9742941d22532f93bbbc65c4f118
dhawalshankar <dhawalmannu@gmail.com>
2026-09-18 11:56:30 +0530

    fix: head-tags

--- docusaurus.config.mjs
+++ docusaurus.config.mjs
@@ -14,6 +14,7 @@ const config = {
     },
     {
       tagName: 'script',
+      attributes: {},
       innerHTML: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
